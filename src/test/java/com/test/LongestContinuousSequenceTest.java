d1e24f29b94031d65896228d0ca25377539ddc47
ashokmundru <ashokm.rp@gmail.com>
2024-07-12 21:52:15 +0800

    Test cases improved

--- src/test/java/com/test/LongestContinuousSequenceTest.java
+++ src/test/java/com/test/LongestContinuousSequenceTest.java
@@ -10,8 +10,20 @@ public class LongestContinuousSequenceTest {
     void maxContinuousOnes() {
         assertEquals(4, LongestContinuousSequence.getStartPositionOfLongestSequenceOfOnes(156));
         assertEquals(3, LongestContinuousSequence.getStartPositionOfLongestSequenceOfOnes(88));
+    }
+
+    @Test
+    void maxContinuousOnesWithZero() {
         assertEquals(-1, LongestContinuousSequence.getStartPositionOfLongestSequenceOfOnes(0));
+    }
+
+    @Test
+    void maxContinuousOnesWithAllOnes() {
         assertEquals(1, LongestContinuousSequence.getStartPositionOfLongestSequenceOfOnes(7));
+    }
+
+    @Test
+    void maxContinuousOnesWithNegativeInput() {
         assertThrows(IllegalArgumentException.class, () -> LongestContinuousSequence.getStartPositionOfLongestSequenceOfOnes(-1));
     }
 }
--- src/test/java/com/test/WordFinderTest.java
+++ src/test/java/com/test/WordFinderTest.java
@@ -6,11 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WordFinderTest {
     @Test
-    void findWordWithMostOccurences() {
+    void findWordWithMostOccurencesWithOnlyLetterExistingWord() {
         assertEquals("crazy", WordFinder.findWordWithMostOccurences("This is a very long sentence and I want to educate everyone in this whole crazy world...", 'z'));
+    }
+
+    @Test
+    void findWordWithMostOccurencesWithCaseSensitiveWord() {
         assertEquals("I", WordFinder.findWordWithMostOccurences("This is a very long sentence and I want to educate everyone in this whole crazy world...", 'I'));
+    }
+
+    @Test
+    void findWordWithMostOccurencesWithMoreThanOneWord() {
         assertEquals("sentence", WordFinder.findWordWithMostOccurences("This is a very long sentence and I want to educate everyone in this whole crazy world...", 'e'));
+    }
+
+    @Test
+    void findWordWithMostOccurencesWithNull() {
         assertEquals("", WordFinder.findWordWithMostOccurences("This is a very long sentence and I want to educate everyone in this whole crazy world...", 'x'));
 
     }
+
 }
